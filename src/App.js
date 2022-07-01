7f9a12d237080f5707167f228f5405ba39c0aa76
Afolabi Opeyemi <102289242+greenlife-developer@users.noreply.github.com>
2022-07-01 10:16:41 -0700

    Added video background for the banner Adding changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Nav from "./components/Home/Nav";
+import Banner from "./components/Home/Banner";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      {/* <Home /> */}
     </div>
   );
 }
--- src/components/Home/Nav.jsx
+++ src/components/Home/Nav.jsx
@@ -1,22 +1,41 @@
 import React from "react";
 import logo from "../../images/logo.png";
+import bgVideo from "../../images/videos/bgvideo.mp4";
 import "./nav.css";
 
 export default function Nav() {
   return (
-    <nav className="navigation">
-      <div className="logo">
-        <img src={logo} alt="" />
-      </div>
-      <ul>
-        <li>
-          <a href="/">Login</a>
-        </li>
-        <li>
-          <a href="/">Register</a>
-        </li>
-        <li></li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navigation">
+        <video src={bgVideo} autoPlay muted loop className="myVideo">
+          <source type="video/mp4" />
+          Your browser does not support HTML5 video.
+        </video>
+        <div className="nav-content">
+          <div className="nav">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            <ul>
+              <li>
+                <a href="/">Login</a>
+              </li>
+              <li>
+                <a href="/">Register</a>
+              </li>
+              <li></li>
+            </ul>
+          </div>
+          <div className="banner-content">
+            <div className="welcome-text">
+              <h1>welcome to eversion</h1>
+            </div>
+            <div className="welcome-img">
+              <img src="" alt="welcome-img" />
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
